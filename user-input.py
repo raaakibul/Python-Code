5c7971fbd35dfd82ce9face4add7730f23db76dc
rakibul-islam <raakibul.uap@gmail.com>
2024-06-25 22:59:57 +0600

    user input python file added

---
+++ user-input.py
@@ -0,0 +1,14 @@
+number1 = int(input("Enter first number:"))
+
+number2 = int(input("Enter second number:"))
+
+add = number1 + number2
+print("Addition: ",add)
+
+name = input("Enter name:")
+age = int(input("Enter age:"))
+gpa = input("Enter gpa:")
+
+print("Name: ",name)
+print("Age: ",age)
+print("GPA: ",gpa)
