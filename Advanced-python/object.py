2ba4b31c4fb9d754b2ea4b2f13bdc1a470cacb51
rakibul-islam <raakibul.uap@gmail.com>
2024-06-26 01:30:04 +0600

    object code added

--- Advanced-python/object.py
+++ Advanced-python/object.py
@@ -10,9 +10,13 @@ class Person:
         print("Height:", height)
         print("\n")
     
-    def __del__(self):
-        print("object deleted")
+    # def __del__(self):
+    #     print("object deleted")
+    
+    def __str__(self):
+        return "name:{}, Age:{}, Height:{}".format(self.name, self.age, self.height)
 
 p1 = Person("Mike", 23, 180)
+print(p1)
 p2 = Person("Bob",34,145)
 p3 = Person("Trot", 45,156)
