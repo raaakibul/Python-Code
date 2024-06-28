ee0912ccf94271d7b5d232ba9bdc2b344d90e45f
rakibul-islam <raakibul.uap@gmail.com>
2024-06-28 21:10:41 +0600

    boolean operator

--- math.py
+++ math.py
@@ -3,3 +3,14 @@ print(max(10,220))
 print(min(20,10))
 print(abs(-4))
 print(pow(2,3))
+print(round(9.45678))
+
+num1 = 10 
+num2 = 10 
+print("Rakibul Islam", end='')
+print(f"{num1} + {num2} = {num1 + num2}")
+
+# boolean operator
+# True and False 
+result = 30 >20
+print(result)
