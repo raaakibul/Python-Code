a5c88982235afa9a866f6c0062dc2e4e9edd8d44
rakibul-islam <raakibul.uap@gmail.com>
2024-06-28 21:56:33 +0600

    sum of n numbers

--- while.py
+++ while.py
@@ -3,4 +3,13 @@ while i<=10:
     print(i)
     i = i+1
 print("end")
-    
+
+# sum of n numbers 
+
+n = int(input("Enter the number:"))
+sum = 0
+i =1 
+while i<=n:
+    sum = sum + i
+    i = i+1
+print(sum)
