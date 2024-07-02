58fc98fe1027686dfe288a4dfcd2480ac5399c2f
rakibul-islam <raakibul.uap@gmail.com>
2024-07-02 16:31:20 +0600

    List basics

--- List.py
+++ List.py
@@ -7,8 +7,12 @@ print(subjects[2:])
 print(subjects[-1])
 
 print("Python" in subjects)
-
 print("c--" in subjects)
+print(len(subjects))
+
+subjects.append("Toc")
+print(subjects)
+
 n = input("Enter the list of numbers:")
 list = n.split()
 sum =0
