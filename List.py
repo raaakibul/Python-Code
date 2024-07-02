8b2c4a9634b0750093f3967e63c3efc0aeb1457c
rakibul-islam <raakibul.uap@gmail.com>
2024-07-02 13:23:24 +0600

    List basics

--- List.py
+++ List.py
@@ -1,3 +1,14 @@
+# list is the object of python
+subjects = ["C", "C++", "Python", "Java", "JavaScript"]
+
+print(subjects)
+print(subjects[0])
+print(subjects[2:])
+print(subjects[-1])
+
+print("Python" in subjects)
+
+print("c--" in subjects)
 n = input("Enter the list of numbers:")
 list = n.split()
 sum =0
