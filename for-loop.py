f8589a6977bdf0b94c733d7603b81d06e3a8fae5
rakibul-islam <raakibul.uap@gmail.com>
2024-07-04 09:50:39 +0600

    using for loop

---
+++ for-loop.py
@@ -0,0 +1,13 @@
+num = [10,20,30,40,50]
+print(num)
+
+# index = 0
+# n = len(num)
+# while index < n:
+#     print(num[index])
+#     index += 1
+# for loop
+
+print("Using for loop:")
+for x in num:
+    print(x)
