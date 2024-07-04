14cd3cdcf1288b23c709129f16fd9669f5e4a72c
rakibul-islam <raakibul.uap@gmail.com>
2024-07-04 09:44:39 +0600

    range function

---
+++ Range-function.py
@@ -0,0 +1,16 @@
+subjects = [20,10,4,4,4]
+pos = subjects.count(4)
+print(pos)
+
+# generate number
+num = list(range(100))
+print(num)
+print(num[2])
+print(num[-1])
+
+# 2 parameters
+num = list(range(5,15))
+print(num)
+# 3 parameters
+num = list(range(2,101,2))
+print(num)
