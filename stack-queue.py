c818f22206942037b95de0b3bff0073358fb5058
rakibul-islam <raakibul.uap@gmail.com>
2024-07-07 19:46:20 +0600

    pop and push

--- stack-queue.py
+++ stack-queue.py
@@ -7,3 +7,13 @@ print(books)
 
 books.pop()
 print("Now the top book is : ", books[-1])
+
+
+# queue
+from collections import deque
+
+bank = deque(["C", "Java", "Javascript"])
+print(bank)
+bank.popleft()
+print(bank)
+
