ac673d2941b40c8e2001156032f68d8010d40965
rakibul-islam <raakibul.uap@gmail.com>
2024-07-10 19:33:12 +0600

    xarguments and xxarguments

--- arguments.py
+++ arguments.py
@@ -10,3 +10,17 @@ def person(*details):
 
 person(101, "A")
 person(102, "B", "Rakibul")
+
+def add(*numbers):
+    sum =0
+    for num in numbers:
+        sum += num
+    print(sum)
+    
+add(101, 120)
+add(100,1001,10001)
+
+def students(**details):
+    print(details)
+    
+students(id=101,name="Student")
