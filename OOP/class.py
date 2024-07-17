96b487a5744adcd05765851d0d88a41c3949b263
rakibul-islam <raakibul.uap@gmail.com>
2024-07-17 22:01:19 +0600

    class

--- OOP/class.py
+++ OOP/class.py
@@ -4,8 +4,12 @@ class Student:
     gpa = ""
 
 s1 = Student()
+s2 = Student()
 print(isinstance(s1, Student))
 
 s1.roll = "123456"
 s1.gpa ="3.60"
+s2.roll = "123457"
+s2.gpa = "3.70"
 print(f"Roll: {s1.roll}, GPA: {s1.gpa}")
+print(f"Roll: {s2.roll}, GPA: {s2.gpa}")
