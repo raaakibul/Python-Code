534aff034cf7161905124ff6be39ca0807e766a1
rakibul-islam <raakibul.uap@gmail.com>
2024-10-19 22:34:26 +0600

    user input with variable

---
+++ Beginners/user-input.py
@@ -0,0 +1,7 @@
+name = input("Enter your name: ")
+age = int(input("Enter your age: "))
+salary = float(input("Enter your salary: "))
+
+print("Name : ", name)
+print("Age : ", age)
+print("Salary : ", salary)
