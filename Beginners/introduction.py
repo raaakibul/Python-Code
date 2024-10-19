31ff7a1e2f52f161dfce6e02e00f4476117c0df2
rakibul-islam <raakibul.uap@gmail.com>
2024-10-19 22:30:24 +0600

    Python Basic code

--- Beginners/introduction.py
+++ Beginners/introduction.py
@@ -2,3 +2,13 @@ print("Hello world!")
 # Backslash characters
 print("Backslash characters")
 print("Name\n: Rakibul Islam")
+
+# variable and data types 
+name = "Abdul Rahim"
+print("Name : "+name)
+
+number = 12354
+print("Number : "+number)
+
+number2 = 20112121.212
+print("Float Number : "+number2)
