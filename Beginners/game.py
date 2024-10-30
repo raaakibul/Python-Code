89680650463b564dc98bab77fe2edb45c550b9e3
rakibul-islam <raakibul.uap@gmail.com>
2024-10-30 23:27:56 +0600

    guess number

--- Beginners/game.py
+++ Beginners/game.py
@@ -1,9 +1,12 @@
 from random import randint
 
-guessNumber = int(input("Enter the number between 1 to 5:"))
-randomNumber = randint(1, 5)
-
-if guessNumber == randomNumber:
-    print("Won")
-else:
-    print("Failed")
+for x in range(1,6):
+    guessNumber = int(input("Enter the number between 1 to 5:"))
+    randomNumber = randint(1, 5)
+    
+    if guessNumber == randomNumber:
+        print("Won")
+    else:
+        print("Failed")
+        print("Number was : ", randomNumber)
+        
