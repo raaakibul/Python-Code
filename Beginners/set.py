8997372b7d6aed8fb9d631792670855759d608f3
rakibul-islam <raakibul.uap@gmail.com>
2024-11-02 22:30:15 +0600

    set

---
+++ Beginners/set.py
@@ -0,0 +1,14 @@
+num = {1,2,3,4,5}
+num2 =set([4,5,6])
+print(num)
+
+print(num2)
+num2.add(7)
+print(num2)
+num2.remove(7)
+print(num2)
+print(4 in num)
+
+print(num | num2)
+print(num & num2)
+print(num - num2)
