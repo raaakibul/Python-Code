7b492a99fc3b3c9e46bdeddc742e99333baf7550
rakibul-islam <raakibul.uap@gmail.com>
2024-11-04 22:52:46 +0600

    function

--- Beginners/function.py
+++ Beginners/function.py
@@ -3,4 +3,14 @@
 
 def add(a,b):
     return a + b
+def sub(a,b):
+    return a - b
+def mul(a,b):
+    return a * b
+def div(a,b):
+    return a / b
+
 print("Addition:", add(10,20))
+print("Subtraction:", sub(10,20))
+print("Multiplication:",mul(10,20))
+print("Division:", div(10,20))
